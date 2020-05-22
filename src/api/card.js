f77db2e59454d6ef613418eca06c88d62bdf6e52
Mateus Souza <mateus_msouza@outlook.com>
2020-05-21 23:58:57 -0300

    partial home

--- src/api/card.js
+++ src/api/card.js
@@ -3,14 +3,13 @@ import { URL_SERVER } from "./server";
 
 export const createCard = card => {
     const key = localStorage.getItem("key");
-
+    console.log('key used', key);
     return axios.post(`${URL_SERVER}/card`, {
         name: card.name,
         description: card.description,
         blockerCard: card.blockerCard
     }, 
     {headers: {
-        'Content-Type': 'json/application',
         Authorization: `Bearer ${key}`
     }})
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,39 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { createCard as create } from "../store/ducks/card";
+import { FormControl, FormHelperText, Button, TextField } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const useStyles = makeStyles({
-    background:{
+    background: {
         height: '100vh',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column'
         
+    },
+    displayFlexCentered: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+    textField: {
+        marginLeft: 8,
+        marginRight: 8,
+        marginTop: 8,
+        marginBottom: 8
     }
 
 })
 
 const Home = props => {
-    var classes = useStyles();
+    const classes = useStyles();
+    const [cardName, setCardName] = useState('')
+    const [cardDescription, setCardDescription] = useState('')
+    const [cardBlocker, setCardBlocker] = useState(undefined) 
     const items = [1, 2, 3, 4];
 
     const createCard = e => {
         const card = {
-            name: "fazer delete",
-            description: "necessario fazer o resto do car",
-            blockerCard: undefined
+            name: cardName,
+            description: cardDescription,
+            blockerCard: cardBlocker
         }
         props.create(card)
     }
     return (
         <div classes={classes.background}>
-            <div>
-                <input placeholder="card name"></input>
-                <input placeholder="card description"></input>
-                <input placeholder="card blocker"></input>
-                <button onClick={createCard}>Create</button>
+            <div className={classes.displayFlexCentered}>
+                <div >
+                    <TextField
+                        className={classes.textField} 
+                        label="card name" 
+                        required={true}
+                        value={cardName}
+                        onChange={e => setCardName(e.target.value)}/>
+                    
+                    <TextField
+                        className={classes.textField}
+                        label="card description" 
+                        required={true}
+                        value={cardDescription}
+                        onChange={e => setCardDescription(e.target.value)}/>
+                    
+                    <Autocomplete
+                    id="cardBlocker"
+                    options={["1","2","3","4","5"]}
+                    renderInput={(params) => (
+                    <TextField {...params} value={cardBlocker} onChange={e => setCardBlocker(e.target.value)} label="Card Blocker" margin="normal" variant="outlined" />
+                    )}/>
+                </div>
+                <Button 
+                    onClick={createCard}
+                    color="primary" 
+                    variant="contained">
+                        Create
+                </Button>
             </div>
 
             {items.map( item => <h1 key={item}>item</h1>)}
