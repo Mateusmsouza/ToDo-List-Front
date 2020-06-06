ac6106be22682a5eeaf3400e32c1624a92f74fb3
Mateus Souza <mateus_msouza@outlook.com>
2020-06-05 21:48:10 -0300

    change url server

--- src/api/server.js
+++ src/api/server.js
@@ -1,2 +1,1 @@
-//export const URL_SERVER = "https://todolistservice.herokuapp.com/"
-export const URL_SERVER = "http://127.0.0.1:8080"
+export const URL_SERVER = "https://todolistservice.herokuapp.com/";
