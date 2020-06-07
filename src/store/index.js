1706f13d851a6d0c1787dc791e072028bd91c4bf
Mateus Souza <mateus_msouza@outlook.com>
2020-06-07 13:42:34 -0300

    remove extension redux tools

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ import thunk from "redux-thunk";
 
 
 const enhancer = () => {
-    return compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    return compose(applyMiddleware(thunk));
 }
 
 const store = createStore(reducers, enhancer());
